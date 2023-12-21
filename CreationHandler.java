9e4c3699ff2646f3ee9fd44e7850c6603c80a349
João Válter Arruda <joaovalterarruda@gmail.com>
2023-12-21 21:59:59 -0100

    correção de acordo com o diagrama de classes

---
+++ CreationHandler.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreationHandler {
+    private final List<Medicamento> medicamentos = new ArrayList<>();
+    private final List<InteracaoAlimentar> interacaoAlimentars = new ArrayList<>();
+    private final List<SubstanciaAtiva> substanciaAtivas = new ArrayList<>();
+
+    public void criarMedicamento(){
+
+    }
+    public void criarInteracao(){
+
+    }
+    public void criarSubstancia(){
+
+    }
+
+    public List <SubstanciaAtiva> getListaSubstancias(){
+
+        return null;
+    }
+    public List <InteracaoAlimentar> getListaInteracaoAlimentares(){
+        return null;
+    }
+
+    public List <Medicamento> getListaMedicamentos(){
+        return null;
+    }
+}
