24cd492c6aae0fe4531ffee74578b09c7ac9f748
João Válter Arruda <joaovalterarruda@gmail.com>
2024-01-03 09:12:00 -0100

    adição de três handlers para ser fácil trabalhar

---
+++ CreationHandlerIntAlimentar.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The type Creation handler int alimentar.
+ */
+public class CreationHandlerIntAlimentar {
+    private final List<InteracaoAlimentar> interacaoAlimentars = new ArrayList<>();
+
+    /**
+     * Criar interacao.
+     */
+    public void criarInteracao(){
+
+    }
+
+    /**
+     * Get lista interacao alimentares list .
+     *
+     * @return the list
+     */
+    public List <InteracaoAlimentar> getListaInteracaoAlimentares(){
+        return null;
+    }
+}
---
+++ CreationHandlerSubsAtiva.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The type Creation handler subs ativa.
+ */
+public class CreationHandlerSubsAtiva {
+
+    private final List<SubstanciaAtiva> substanciaAtivas = new ArrayList<>();
+
+
+    /**
+     * Criar substancia.
+     */
+    public void criarSubstancia(){
+
+    }
+
+    /**
+     * Get lista substancias list .
+     *
+     * @return the list
+     */
+    public List <SubstanciaAtiva> getListaSubstancias(){
+
+        return null;
+    }
+
+
+}
